0c554dfabe992f8559625c710941754534e7a112
Daniel Neal <danielneal@Daniels-Air.home>
2020-07-02 11:40:58 +0100

    Add images

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Suspense } from 'react';
-import { StyleSheet, TextInput, Text, View, SafeAreaView, ActivityIndicator,ScrollView } from 'react-native';
+import { StyleSheet,
+         TextInput,
+         Text,
+         View,
+         SafeAreaView,
+         ActivityIndicator,
+         ScrollView,
+         TouchableOpacity,
+         Image} from 'react-native';
 import * as style from "./style"
 import { request } from 'graphql-request'
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer , useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useWindowDimensions } from 'react-native';
 
 import {
     RecoilRoot,
@@ -12,6 +21,7 @@ import {
     selector,
     useRecoilState,
     useRecoilValue,
+    selectorFamily
 } from 'recoil';
 
 const textState = atom({
@@ -21,14 +31,18 @@ const textState = atom({
 
 const endpoint = "https://next.riverford.co.uk/graphql"
 
-const query = /* GraphQL */ `
+const searchQuery = /* GraphQL */ `
     query recipe_search($q: String!,$page_size: Int) {
        recipe_search(q:$q,page_size:$page_size) {
          total_hits
          hits {
             recipe {
+               id
+               slug
                name
                media {
+                  height
+                  width
                   uri
                }
             }
@@ -39,11 +53,35 @@ const query = /* GraphQL */ `
     }
   `
 
+const recipeQuery = /* GraphQL*/ `
+query recipe($slug: String!) {
+       recipe(slug:$slug) {
+           id
+           introduction
+           name
+           media {
+              height
+              width
+              uri
+           }
+         }
+       }
+`
 
 function RecipeHit({recipe}) {
-        return <View style={{padding:style.s3}}>
-          <Text>{recipe.name }</Text>
-        </View>
+    const navigation=useNavigation()
+    const image=recipe.media.find((img)=>img.height<150 &&img.height > 50)
+    if(!image) {
+        return <View/>
+    }
+    else {
+        return <TouchableOpacity key={recipe.slug}
+                  style={{padding:style.s3,flexDirection:"row"}}
+                  onPress={()=>navigation.navigate('Recipe',{slug:recipe.slug})}>
+            <Image source={{uri:image.uri}} style={{width:style.s7,height:style.s7,marginRight:style.s3}}/>
+            <Text style={{fontSize:style.f3}}>{recipe.name}</Text>
+            </TouchableOpacity>
+    }
 }
 
 function Input() {
@@ -53,8 +91,8 @@ function Input() {
     };
 
     return (
-            <View>
-            <TextInput style= {{padding:style.s3}} type="text" onChangeText={onChange} />
+            <View style={{paddingVertical:style.s3}}>
+            <TextInput style={{padding:style.s3,fontSize:style.f3}} placeholder="Search for Recipes" type="text" onChangeText={onChange} />
         </View>
     );
 }
@@ -63,11 +101,19 @@ const hitsState = selector({
     key:`hitsState`,
     get: async ({get}) => {
         const text = get(textState)
-        const result=await request(endpoint,query, {q:text,page_size:10})
+        const result=await request(endpoint,searchQuery, {q:text,page_size:10})
         return result.recipe_search.hits
     }
 })
 
+const recipeState = selectorFamily({
+    key: 'recipeState',
+    get: (slug) => async ({get}) => {
+        const result=await request(endpoint,recipeQuery,{slug:slug});
+        return result;
+    },
+
+});
 
 function QueryResults() {
     const hits=useRecoilValue(hitsState)
@@ -76,7 +122,24 @@ function QueryResults() {
     </ScrollView>
 }
 
-function Search() {
+function Recipe({slug}) {
+    const result=useRecoilValue(recipeState(slug))
+    const dimensions=useWindowDimensions()
+    const image=result.recipe.media.find((img)=>img.height<800 && img.height > 400)
+    return <ScrollView>
+        <Text style={{fontSize:style.f3}}>{result.recipe.name}</Text>
+        <Image source={{uri:image.uri}} style={{width:dimensions.width,height:dimensions.width}}/>
+        <Text>{result.recipe.introduction}</Text>
+        </ScrollView>
+}
+
+function RecipeScreen({route}) {
+    return  <Suspense fallback={<ActivityIndicator/>}>
+        <Recipe slug={route.params.slug}></Recipe>
+        </Suspense>
+}
+
+function SearchScreen() {
     return (
             <SafeAreaView>
             <Input />
@@ -95,7 +158,8 @@ export default function App() {
             <RecoilRoot>
             <NavigationContainer>
             <Stack.Navigator>
-              <Stack.Screen name="Search" component={Search} />
+            <Stack.Screen name="Search" component={SearchScreen} />
+            <Stack.Screen name="Recipe" component={RecipeScreen} />
             </Stack.Navigator>
             </NavigationContainer>
             </RecoilRoot>
